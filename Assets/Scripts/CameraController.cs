0c75be03294ba1694e5a2f78d046698816e10eac
blankhorizon <pthaing5424@gmail.com>
2017-12-11 23:16:12 -0500

    Generating and deleting platforms

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Player thePlayer;
+
+    private Vector3 lastPlayerPosition;
+    private float distanceToMoveX;
+    private float distanceToMoveY;
+
+
+    void Start()
+    {
+        //thePlayer = FindObjectOfType<PlayerController>();
+        thePlayer = FindObjectOfType<Player>();
+        lastPlayerPosition = thePlayer.transform.position;
+    }
+
+    void Update()
+    {
+        distanceToMoveX = thePlayer.transform.position.x - lastPlayerPosition.x;
+        distanceToMoveY = thePlayer.transform.position.y - lastPlayerPosition.y;
+
+        transform.position = new Vector3(transform.position.x + distanceToMoveX, transform.position.y + distanceToMoveY, transform.position.z);
+
+        lastPlayerPosition = thePlayer.transform.position;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    float moveSpeed;
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D myRigidBody;
+
+    public bool grounded;
+    public LayerMask whatIsGround;
+
+    private Collider2D myCollider;
 
 	// Use this for initialization
 	void Start () {
         moveSpeed = 5;
+
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myCollider = GetComponent<Collider2D>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-
-	}
-
     private void FixedUpdate()
     {
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
@@ -26,6 +35,14 @@ public class Player : MonoBehaviour {
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W))
+        {
+            if (grounded)
+            {
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
+            }
         }
     }
 }
